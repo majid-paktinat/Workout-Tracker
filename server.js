72a28dc0a77733942a6ee9c908898dd26cc2f8c5
majid <majid.paktinat@gmail.com>
2020-03-20 19:22:31 -0400

    models created

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // app.post("/submit", ({ body }, res) => {
 //   User.create(body)
